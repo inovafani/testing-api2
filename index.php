bf5801071c8ad12735c9ad49c1ee94ff9a37bd88
Inov Afani <inoveka@gmail.com>
2025-05-06 20:03:51 +0700

    First upload

---
+++ index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+
+    <!-- Bootstrap CSS -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+      crossorigin="anonymous"
+    />
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="css/style.css" />
+
+    <title>My Portofolio</title>
+  </head>
+  <body class="mt-5">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="#">Inov Afani</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#"
+              >Home <span class="sr-only">(current)</span></a
+            >
+            <a class="nav-item nav-link" href="#">About</a>
+            <a class="nav-item nav-link" href="#">Portofolio</a>
+            <a class="nav-item nav-link disabled" href="#">Contact</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container text-center">
+        <img
+          src="img/inov.jpg"
+          class="rounded-circle img-thumbnail"
+          width="25%"
+        />
+        <h1 class="display-4">Inov Afani Eka Putra</h1>
+        <p class="lead">Welcome to my portofolio</p>
+      </div>
+    </div>
+
+    <section id="about" class="about">
+      <div class="container">
+        <div class="row text-center mb-4">
+          <div class="col">
+            <h2>About</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab vero
+              eos possimus sit dicta officiis maiores est ipsam quia, a
+              praesentium id dolores magnam. Culpa quaerat enim ipsam! Commodi,
+              expedita!
+            </p>
+          </div>
+          <div class="col">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A culpa
+              ducimus quae, assumenda exercitationem tempore quisquam quibusdam
+              quos corporis quis ea, sed dolorum? Reiciendis illo dolorem ipsam
+              itaque, id nam.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Youtube & IG -->
+    <section></section>
+
+    <section id="porto" class="porto bg-light pb-4">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>Portofolio</h2>
+          </div>
+        </div>
+
+        <div class="row mb-3">
+          <div class="col-md">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="img/thumbs/1.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="img/thumbs/2.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="img/thumbs/3.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row mb-3">
+          <div class="col-md">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="img/thumbs/4.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="img/thumbs/5.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md">
+            <div class="card">
+              <img
+                class="card-img-top"
+                src="img/thumbs/6.png"
+                alt="Card image cap"
+              />
+              <div class="card-body">
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="contact" class="contact pb-4 mb-4">
+      <div class="container">
+        <div class="row pt-4 mb-4">
+          <div class="col text-center">
+            <h2>Contact Me</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-lg-4">
+            <div class="card text-white bg-primary mb-3">
+              <div class="card-body text-center">
+                <h5 class="card-title">Contact Me</h5>
+                <p class="card-text">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tenetur illo blanditiis quia distinctio! Pariatur odit
+                  inventore qui, ipsum doloribus non sapiente nihil.
+                </p>
+              </div>
+            </div>
+            <ul class="list-group">
+              <li class="list-group-item"><h3>Location</h3></li>
+              <li class="list-group-item">My Office</li>
+              <li class="list-group-item">Jl Telaga Warna No 98, Malang</li>
+              <li class="list-group-item">East Java, Indonesia</li>
+            </ul>
+          </div>
+
+          <div class="col-lg-6">
+            <form>
+              <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama" />
+              </div>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control" id="email" />
+              </div>
+              <div class="form-group">
+                <label for="telp">No telp</label>
+                <input type="text" class="form-control" id="telp" />
+              </div>
+              <div class="form-group">
+                <label for="pesan">Pesan</label>
+                <textarea
+                  name="pesan"
+                  class="form-control"
+                  id="pesan"
+                ></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary">
+                Kirim Pesan!
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="bg-dark text-white">
+      <div class="container">
+        <div class="row pt-3">
+          <div class="col text-center">
+            <p>Copyright 2025</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+      integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+      integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
